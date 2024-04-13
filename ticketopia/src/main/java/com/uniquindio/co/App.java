05e414a3724b44e13e3849c385e6864772a5e9ea
EddSanchezz <edwarde.sanchezm@uqvirtual.edu.co>
2024-04-13 15:22:27 -0500

    Update Base

---
+++ ticketopia/src/main/java/com/uniquindio/co/App.java
@@ -0,0 +1,13 @@
+package com.uniquindio.co;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
